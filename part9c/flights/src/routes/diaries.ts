b0c40517794e3d4d1a45abe63ce70f92ab9f6312
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-10 12:15:08 +1300

    Implement routes & services

---
+++ part9c/flights/src/routes/diaries.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { Response } from 'express';
+import { NonSensitiveDiaryEntry } from '../types';
+import diaryService from '../services/diaryService';
+
+const router = express.Router();
+
+router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
+  res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.post('/', (_req, res) => {
+  res.send('Saving a diary!');
+});
+
+export default router;
