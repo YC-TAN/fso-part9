3e4533ee4fbe2366abba9c17969266d203807094
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-03-11 09:03:44 +1300

    Implement union discrimination

--- part9d/courseTS/src/components/Content.tsx
+++ part9d/courseTS/src/components/Content.tsx
@@ -1,24 +1,74 @@
+import type { CoursePart } from "../types";
+
 interface PartProps {
-    name: string;
-    exerciseCount: number;
+    part: CoursePart;
 }
 
 interface ContentProps {
-    parts: PartProps[];
+    parts: CoursePart[];
 }
 
-const Line = (props: PartProps) => {
-    return (
-    <p>
-        {props.name} {props.exerciseCount}
-    </p>
-    )
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Part = (props: PartProps) => {
+
+    const render = (part: CoursePart) => {
+        switch (part.kind) {
+            case 'basic':
+                return (
+                    <p>
+                        <strong>{part.name} {part.exerciseCount}</strong>
+                        <br/>
+                        <i>{part.description}</i>
+                    </p>
+                        )
+                
+            case 'group':
+                return (
+                    <p>
+                        <strong>{part.name} {part.exerciseCount}</strong>
+                        <br/>
+                        project exercises {part.groupProjectCount}
+                    </p>
+                        )
+            case 'background':
+                return (
+                    <p>
+                        <strong>{part.name} {part.exerciseCount}</strong>                         
+                        <br/>
+                        <i>{part.description}</i>
+                        <br/>
+                        submit to {part.backgroundMaterial}
+                    </p>
+                        )
+            case 'special':
+                return (
+                    <p>
+                        <strong>{part.name} {part.exerciseCount}</strong>                         
+                        <br/>
+                        <i>{part.description}</i>
+                        <br/>
+                        required skills: {part.requirements.join(', ')}
+                    </p>
+                        )
+            default:
+                 return assertNever(part);    
+        }
+    }
+    return (render(props.part))
 }
 
 const Content = (props: ContentProps) => {
   return (
-    props.parts.map((p, idx) => (
-        <Line name={p.name} exerciseCount={p.exerciseCount} key={idx}/>
+    props.parts.map((p) => (
+        <Part part={p} key={p.name}/>
     ))
   )
 }
